2baf1961ad9b5eb01f143f0ffdce8a8b418fbcf1
admin <ongbaujav@gmail.com>
2026-07-29 22:46:01 +0700

    update

--- src/router.jsx
+++ src/router.jsx
@@ -1,22 +1,27 @@
-import {
-    createBrowserRouter
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
 import NovelList from "./pages/NovelList";
 import ChapterList from "./pages/ChapterList";
 import ReaderPage from "./pages/ReaderPage";
 
-export default createBrowserRouter([
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <NovelList />
+        },
+        {
+            path: "/novels/:id",
+            element: <ChapterList />
+        },
+        {
+            path: "/novels/:novelId/chapters/:chapterId",
+            element: <ReaderPage />
+        }
+    ],
     {
-        path: "/",
-        element: <NovelList />
-    },
-    {
-        path: "/novels/:id",
-        element: <ChapterList />
-    },
-    {
-        path: "/novels/:novelId/chapters/:chapterId",
-        element: <ReaderPage />
+        basename: "/novel-tool"
     }
-]);
+);
+
+export default router;
